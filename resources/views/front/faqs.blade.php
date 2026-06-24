46f1c0b869d006d40acfa90448f908472d063db4
vw-uday <verloop.dev4@gmail.com>
2026-06-24 18:06:04 +0530

    code add

---
+++ resources/views/front/faqs.blade.php
@@ -0,0 +1,176 @@
+@extends('layouts.front')
+@push('css')
+<link href="{{ asset('front/css/custom.css') }}" rel="stylesheet" type="text/css" />
+@endpush
+@push('style-css')
+@endpush
+@section('content')
+<section class="page-hero-section">
+    <div class="page-hero-section-overlay bg--green-100 bg--scroll">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="left-column">
+                        <span class="section-id"></span>
+                        <h2 class="s-28 mb-5">Frequently Asked Questions</h2>
+                        <p class="s-16 mt-0">Find quick answers to common questions about loans,
+                            eligibility, documents, approvals, and more.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="faqs-3" class="py-80 faqs-section">
+    <div class="container">
+        <div class="faqs-3-questions">
+            <div class="row justify-content-center">
+                <div class="col-lg-12 col-xl-12">
+                    <div class="accordion-wrapper">
+                        <ul class="accordion">
+                            <li class="accordion-item mb-0">
+                                <div class="accordion-thumb">
+                                    <h6 class="s-16 w-500">1: What types of expenses can a personal loan cover?</h6>
+                                </div>
+                                <div class="accordion-panel">
+                                    <div class="accordion-panel-item">
+                                        <div class="faqs-2-answer">
+                                            <p>A personal loan can be used to cover almost any personal financial need,
+                                                such as medical bills, education, home improvements, and debt
+                                                consolidation.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="accordion-item mb-0">
+                                <div class="accordion-thumb">
+                                    <h6 class="s-16 w-500">2: What are the eligibility requirements for a personal loan?</h6>
+                                </div>
+                                <div class="accordion-panel">
+                                    <div class="accordion-panel-item">
+                                        <div class="faqs-2-answer">
+                                            <p>The eligibility requirements for a personal loan are as follows:</p>
+                                            <p><strong>For Salaried Professionals:</strong></p>
+                                            <div class="ps-2">
+                                                <p>&bull; Minimum Age: 21 Years </p>
+                                                <p>&bull; Minimum Salary: Rs.15,000 Per Month (Should reflect in bank
+                                                    statement) </p>
+                                                <p>&bull; Minimum Job Duration: 1 Year</p>
+                                            </div>
+                                            <p><strong>For Self-Employed Individuals:</strong></p>
+                                            <div class="ps-2">
+                                                <p>&bull; Minimum Age: 21 Years</p>
+                                                <p>&bull; Income Tax Return Of Minimum 1 Year</p>
+                                                <p>&bull; Minimum Business Duration: 1 Year</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="accordion-item mb-0">
+                                <div class="accordion-thumb">
+                                    <h6 class="s-16 w-500">3: What are the documents needed to apply for a personal loan?
+                                    </h6>
+                                </div>
+                                <div class="accordion-panel">
+                                    <div class="accordion-panel-item">
+                                        <div class="faqs-2-answer">
+                                            <p>The following are the general documents required when applying for a
+                                                personal loan.</p>
+                                            <p><strong>For Salaried:</strong></p>
+                                            <div class="ps-2">
+                                                <p>&bull; Aadhaar Card</p>
+                                                <p>&bull; PAN Card</p>
+                                                <p>&bull; Residence Proof: Rent agreement or Utility bills</p>
+                                                <p>&bull; Bank Statement</p>
+                                                <p>&bull; Income Proof: Salary Slips or Form 16</p>
+                                            </div>
+                                            <p><strong>For Self-Employed:</strong></p>
+                                            <div class="ps-2">
+                                                <p>&bull; Aadhaar Card</p>
+                                                <p>&bull; PAN Card</p>
+                                                <p>&bull; Residence Proof: Rent agreement or Utility bills</p>
+                                                <p>&bull; Bank Statement</p>
+                                                <p>&bull; Balance Sheet</p>
+                                                <p>&bull; Income Computation</p>
+                                                <p>&bull; Service Tax Registration, License, Registration Certificate
+                                                </p>
+                                                <p>&bull; Income Tax Returns</p>
+                                            </div>
+                                            <p>Based on your profile and the guidelines, the lender may request
+                                                additional documents.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="accordion-item mb-0">
+                                <div class="accordion-thumb">
+                                    <h6 class="s-16 w-500">4. Is credit score a factor in personal loan approval?</h6>
+                                </div>
+                                <div class="accordion-panel">
+                                    <div class="accordion-panel-item">
+                                        <div class="faqs-2-answer">
+                                            <p>Yes, your credit score is an important factor in the loan application
+                                                process. It is one of the most important factors lenders use to
+                                                determine your creditworthiness.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="accordion-item mb-0">
+                                <div class="accordion-thumb">
+                                    <h6 class="s-16 w-500">5. How can I improve my chances of receiving a personal loan?</h6>
+                                </div>
+                                <div class="accordion-panel">
+                                    <div class="accordion-panel-item">
+                                        <div class="faqs-2-answer">
+                                            <p>To improve your chances, maintain a good credit score, have consistent
+                                                income, and provide accurate documentation during the application
+                                                process.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="accordion-item mb-0">
+                                <div class="accordion-thumb">
+                                    <h6 class="s-16 w-500">6. Do personal loans provide any tax benefits?</h6>
+                                </div>
+                                <div class="accordion-panel">
+                                    <div class="accordion-panel-item">
+                                        <div class="faqs-2-answer">
+                                            <p>Personal loans do not offer any direct tax advantages. However, if you
+                                                use a personal loan to invest in your business, renovate your home, or
+                                                fund your education, you may be able to deduct the interest you pay. For
+                                                more information, please contact your CA or tax advisor.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="accordion-item mb-0">
+                                <div class="accordion-thumb">
+                                    <h6 class="s-16 w-500">7. In addition to credit scores, what other factors do lenders
+                                        consider when approving personal loans?</h6>
+                                </div>
+                                <div class="accordion-panel">
+                                    <div class="accordion-panel-item">
+                                        <div class="faqs-2-answer">
+                                            <p>In addition to credit score, lenders consider the applicant's age,
+                                                income, and job stability.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<hr class="divider">
+@endsection
+@push('script-src')
+@endpush
+@push('scripts')
+@endpush
---
+++ resources/views/partials/front/testimonials.blade.php
@@ -0,0 +1,109 @@
+<!-- Testimonials carousel start  -->
+<div class="owl-carousel owl-theme reviews-1-wrapper">
+    <div class="review-1 bg--white-100 shadow-md r-08 p-0">
+        <div class="text-center bg--green-500 pt-4  pb-5 px-4 mb-25">
+            <p class="mt-0 text-white"><i>"As a happy customer of Kreditsathi, I would say that for any quick financial
+                    needs, everyone should select this platform. It is so easy and very fast. "</i></p>
+        </div>
+        <div class="review-txt p-4">
+            <div class="review-avatar float-none mb-3">
+                <img src="{{ asset('front/images/review/reviews-01.jpg') }}" alt="review-avatar" width="auto"
+                    class="m-auto">
+            </div>
+            <div class="author-data clearfix text-center">
+                <div class="review-author text-center">
+                    <h4 class="s-16 w-600 mb-2">Manish Verma</h4>
+                    <div class="star-rating ico-15">
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star-half-empty mr-5"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="review-1 bg--white-100 shadow-md r-08 p-0">
+        <div class="text-center bg--green-500 pt-4  pb-5 px-4 mb-25">
+            <p class="mt-0 text-white"><i>“Though it is 100% online, it is a very fast and reliable process. And the
+                    customer support is
+                    10/10. Highly recommended to all who are looking for a loan. ”</i></p>
+        </div>
+        <div class="review-txt p-4">
+            <div class="review-avatar float-none mb-3">
+                <img src="{{ asset('front/images/review/reviews-02.jpg') }}" alt="review-avatar" width="auto"
+                    class="m-auto">
+            </div>
+            <div class="author-data clearfix text-center">
+
+                <div class="review-author text-center">
+                    <h4 class="s-16 w-600 mb-2">Sanjay Singh</h4>
+                    <div class="star-rating ico-15">
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star-half-empty mr-5"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="review-1 bg--white-100 shadow-md r-08 p-0">
+        <div class="text-center bg--green-500 pt-4  pb-5 px-4 mb-25">
+            <p class="mt-0 text-white"><i>"I really loved how the Kreditsathi team supported me. When I had doubts, they
+                    clarified them and
+                    went above and beyond to ensure that I had no problems. Thank you so much."</i></p>
+        </div>
+        <div class="review-txt p-4">
+            <div class="review-avatar float-none mb-3">
+                <img src="{{ asset('front/images/review/reviews-03.jpg') }}" alt="review-avatar" width="auto"
+                    class="m-auto">
+            </div>
+            <div class="author-data clearfix text-center">
+
+                <div class="review-author text-center">
+                    <h4 class="s-16 w-600 mb-2">Yash Singh Dagar</h4>
+                    <div class="star-rating ico-15">
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star-half-empty mr-5"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="review-1 bg--white-100 shadow-md r-08 p-0">
+        <div class="text-center bg--green-500 pt-4  pb-5 px-4 mb-25">
+            <p class="mt-0 text-white"><i>"My overall experience with Kreditsathi is mind-blowing. Everything was too
+                    smooth and quick. The
+                    staff is also extremely professional, quick to respond, and friendly."</i></p>
+        </div>
+        <div class="review-txt p-4">
+            <div class="review-avatar float-none mb-3">
+                <img src="{{ asset('front/images/review/reviews-04.jpg') }}" alt="review-avatar" width="auto"
+                    class="m-auto">
+            </div>
+
+            <div class="author-data clearfix text-center">
+
+                <div class="review-author text-center">
+                    <h4 class="s-16 w-600 mb-2">Ojas Gupta</h4>
+                    <div class="star-rating ico-15">
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star"></span>
+                        <span class="flaticon-star-half-empty mr-5"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Testimonials carousel ends  -->

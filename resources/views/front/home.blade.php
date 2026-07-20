fa90e6dacdcf6d2ca21109226494ef087cc615be
vw-uday <verloop.dev4@gmail.com>
2026-07-20 10:36:34 +0530

    image issue

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -16,7 +16,7 @@
             <div class="col p-0">
                 <div class="owl-carousel owl-theme reviews-1-wrapper-new">
                     <div class="slide-box">
-                        <img src="{{ asset('front/images/Homepage-1.jpg') }}" class="img-fluid w-100">
+                        <img src="{{ asset('front/images/Home-main-1.jpg') }}" class="img-fluid w-100">
                     </div>
                     <div class="slide-box">
                         <img src="{{ asset('front/images/Homepage-2.jpg') }}" class="img-fluid w-100">

3d971d49523ce8980d086ed83757994dff0e499f
vw-uday <verloop.dev4@gmail.com>
2026-08-07 11:05:59 +0530

    issue solve

--- app/Http/Controllers/SelfApplyController.php
+++ app/Http/Controllers/SelfApplyController.php
@@ -1158,7 +1158,7 @@ class SelfApplyController extends Controller
                 $lastname = strtolower($userData->last_name);
                 $city = strtolower(preg_replace("/[^a-zA-Z]+/", "", $userData->city));
                 $state = strtolower(getStateAbbreviation($userData->state));
-                $orderData = orderdata($orderId, 'subpaisa_entry');
+                $orderData = orderdata($orderId, 'razorpayentry');
 
                 UserRegistration::where('id', $userData->userid)->update(['process_step' => 5]);
 
